a4c5caf708fb411deae937f9975d90f82c988fdd
PavanKumarBollu <bollupavankumar12@gmail.com>
2026-07-24 12:27:26 +0530

    JavaScript

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,48 @@
+// courseName = "MERN_AI";
+// courseDuration = 3.5;
+// totalStudentCount = 38;
+// totalNumberOfCertificates = 4;
+// qualification = "BTech";
+
+// "MERN_AI" String
+// 38 Number Integer
+// 3.4 Number Float
+// TRUE or False boolean values
+
+// undefined
+// null
+
+// javaScript is dynamic typed language means the type of values we are gonna store
+// in the variable are defined(informed) at runtime (while javascript execution )
+//  this feature is possible beacause of that we can feel free to add anykind of value to the variables
+//  javascript is interpreter based language
+//
+
+// let name = "MERN_AI";
+// console.log(name);
+
+// name = 35;
+// console.log(name);
+// name = true;
+// console.log(name);
+
+const courseName = "MERN_AI";
+console.log(courseName);
+courseName = 12;
+console.log(courseName);
+
+const dob = "15-06-2001";
+
+const aadharNumber = "1234 1234 1234";
+
+let mobileNum = 1234512345;
+
+const gender = "Male";
+const surname = "BBBBB";
+country = "INDIAN";
+fatherName = "Krishna";
+let loveName = "AAAA";
+let password = "12341234";
+
+const permantAddres = "NGKL";
+let currentAddress = "Shapur HYD";
---
+++ JavaScript/Variables.js
@@ -0,0 +1,25 @@
+let name = "Pavan";
+let age = 24;
+// CTRL + /
+// const name = "Pavan";
+// const age = 24;
+
+// var name = "Pavan";
+// var age = 24;
+
+let firstJob = "SoftwareEngineer";
+let secondJob = "Facilitator";
+
+let job1 = "Facilitator";
+let job2 = "SoftwareEngineer";
+
+let firstName = "Pavan";
+let lastName = "Kumar";
+
+let mobile = 1234512345;
+
+console.log(name);
+// console.log("Pavan");
+console.log(name);
+console.log(name);
+console.log(name);
